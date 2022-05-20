9a8ed92d4a03525ee7589344b39e700dc1a3a353
sillyangel <sillyangel3@gmail.com>
2022-05-20 14:38:22 -0500

    fix bash

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Set this to 1 to automatically attempt a fix when an error occurs while running npm start.
 fix=0
-npm install
-npm start || [[ $fix = 1 ]] && npm install && npm update && git pull
+
+
+# Stuff to do at first run(submodule, npm install).
+if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
+  npm install
+  git fetch
+  git pull
+fi
+
+
+npm start || [[ $fix = 1 ]] && npm install && npm update && git submodule update --init --recursive
