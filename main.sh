0142809d8df7f14498d14d2e3d9cdd898df44a4b
a <sillyangel3@gmail.com>
2022-05-21 00:12:50 +0000

    buh

--- main.sh
+++ main.sh
@@ -6,9 +6,7 @@ fix=0
 # Stuff to do at first run(submodule, npm install).
 if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
   npm install
-  git fetch
-  git pull
 fi
 
 
-npm start || [[ $fix = 1 ]] && npm install && npm update && git submodule update --init --recursive
+npm start || [[ $fix = 1 ]] && npm install && npm update && git pull
