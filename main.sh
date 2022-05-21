9b3e0e3a1c065ce34534ba263aa92f490c4920fa
a <sillyangel3@gmail.com>
2022-05-21 02:06:47 +0000

    autoupdate

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 # Set this to 1 to automatically attempt a fix when an error occurs while running npm start.                                                                       
 fix=0
 
+git fetch && git pull
 
 # Stuff to do at first run(submodule, npm install).
 if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
