66c59e55b0e6c19ddcbc6e306f3aef24b1ec2da5
sillyangel <sillyangel3@gmail.com>
2022-05-23 14:26:01 -0500

    main.sh

--- main.sh
+++ main.sh
@@ -6,8 +6,14 @@ git fetch && git pull
 
 # Stuff to do at first run(submodule, npm install).
 if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
+  echo installing npm packages
   npm install
+  echo finish
 fi
 
 
-npm start || [[ $fix = 1 ]] && npm install && npm update 
+npm start || [[ $fix = 1 ]] && npm install && npm update && git fetch && git pull 
+
+
+wait 10
+echo "if the problem still occurs make a issue in the offical f3ninge github repo"
