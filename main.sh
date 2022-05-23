6fb7202e97ca791c58df39d7ed2c4f4cef4acb98
sillyangel <sillyangel3@gmail.com>
2022-05-23 15:30:57 -0500

    Better sh

--- main.sh
+++ main.sh
@@ -2,18 +2,18 @@
 # Set this to 1 to automatically attempt a fix when an error occurs while running npm start.                                                                       
 fix=0
 
+# auto update
 git fetch && git pull
 
 # Stuff to do at first run(submodule, npm install).
-if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
+if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]]; || [[ ! -d "static" ]] then
   echo installing npm packages
   npm install
-  echo finish
-fi
-
-
+  echo finished
+fi 
+# Started the proxy and stuff
 npm start || [[ $fix = 1 ]] && npm install && npm update && git fetch && git pull 
 
-
+# Error checking
 wait 10
-echo "if the problem still occurs make a issue in the offical f3ninge github repo"
+echo problem still occurs make a issue inside of the offical f3ninge github repo
